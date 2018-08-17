7a270c9e0553ca4b7193b5208b8466ecbd4885f1
illya.petrusenko <illya.petrusenko@gmail.com>
2018-08-17 18:16:25 +0300

    third

--- monitorings/monitorings.py
+++ monitorings/monitorings.py
@@ -1,7 +1,7 @@
 import random
 
 
-class Warrior:
+class OfficeWarrior:
 
     def __init__(self, name, health, dmg):
         self.name = name
@@ -23,37 +23,44 @@ class Warrior:
         return self.health
 
     def attack(self, enemy):
-
         if enemy.health > 0:
             enemy.health -= self.dmg
-        else:
+        elif enemy.health == 0:
             print('{} уже хорошенько получил монитором по ебалу и не может принять ещё один удар'.format(enemy.name))
 
+        # print('Состояние здоровья офисного воина {}: {}'.format(enemy.name, enemy.health))
         return enemy.health
 
 
-w1 = Warrior('Andrew', 100, 110)
-w2 = Warrior('Nicola', 100, 10)
-
+w1 = OfficeWarrior('Andrew', 100, random.randrange(1, 20))
+w2 = OfficeWarrior('Nicola', 100, random.randrange(1, 20))
 warriors = (w1, w2)
 
-attacker = random.choice(warriors)
-print(attacker.name)
 
-
-def return_enm(number):
-    if number == w2:
-        enm = w1
-
+def select_zad(warriors):
+    first = random.choice(warriors)
+    if first == w1:
+        second = w2
     else:
-        enm = w2
+        second = w1
+    return first, second
 
-    return enm
+
+def fight(first, second):
+    if second.health or first.health != 0:
+        while second.health and first.health > 0:
+
+            if first.dmg > second.health:
+                first.dmg = second.health
+            first.attack(second)
+            print('Офисный крыс: {} получил монитором по ебалу от {} и его здоровье: {}'.format(second.name, first.name, second.health))
+
+            if second.dmg > first.health:
+                second.dmg = first.health
+            second.attack(first)
+            print('Офисный крыс: {} получил монитором по ебалу от {} и его здоровье: {}'.format(first.name, second.name, first.health))
 
 
-enemy = return_enm(attacker)
-print(enemy.name)
-while enemy.health > 0:
-    attacker.attack(enemy)
-
+sz = select_zad(warriors)
+fight(sz[0], sz[1])
 
